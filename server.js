43f65c4859286649116a0bf65982abbb66c184a3
ilyosdev <ilyosaxmadaliy@gmail.com>
2023-07-15 08:24:58 +0500

    //

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ import connectDB from "./src/config/db.js";
 connectDB();
 
 const app = express();
-app.use( express.json() );
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", todosRouters);
 
-const PORT = process.env.PORT || 2020;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
